d525fb5232388d6bb8a78cd0c56af734f64b7171
ibrahimi20 <mohssinibra@gmail.com>
2022-09-07 02:13:27 +0100

    project-initialisation

---
+++ MyE-commerce/frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        <a href="/">Logo_Store</a>
+      </header>
+      <main>product list</main>
+    </div>
+  );
+}
+
+export default App;

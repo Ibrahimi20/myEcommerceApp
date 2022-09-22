4c27c3d5efeb71007f3712ebf7fa8551d54047ce
ibrahimi20 <mohssinibra@gmail.com>
2022-09-22 23:25:20 +0100

    SearchFeature

--- MyE-commerce/backend/routes/ProductRoute.js
+++ MyE-commerce/backend/routes/ProductRoute.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
 import Product from '../models/ProductModel.js';
 const productRoute = express.Router();
 
@@ -6,6 +7,96 @@ productRoute.get('/', async (req, res) => {
   const products = await Product.find();
   res.send(products);
 });
+const PAGE_SIZE = 3;
+productRoute.get(
+  '/search',
+  expressAsyncHandler(async (req, res) => {
+    const { query } = req;
+    const pageSize = query.pageSize || PAGE_SIZE;
+    const page = query.page || 1;
+    const category = query.category || '';
+    const price = query.price || '';
+    const rating = query.rating || '';
+    const order = query.order || '';
+    const searchQuery = query.query || '';
+
+    const queryFilter =
+      searchQuery && searchQuery !== 'all'
+        ? {
+            name: {
+              $regex: searchQuery,
+              $options: 'i',
+            },
+          }
+        : {};
+
+    const categoryFilter = category && category !== 'all' ? { category } : {};
+    const ratingFilter =
+      rating && rating !== 'all'
+        ? {
+            rating: {
+              $gte: Number(rating),
+            },
+          }
+        : {};
+    // $ used for comparison query operators
+    const priceFilter =
+      price && price !== 'all'
+        ? {
+            // 1-50
+            price: {
+              $gte: Number(price.split('-')[0]),
+              $lte: Number(price.split('-')[1]),
+            },
+          }
+        : {};
+
+    const sortOrder =
+      order === 'featured'
+        ? { featured: -1 }
+        : order === 'lowest'
+        ? { price: 1 }
+        : order === 'highest'
+        ? { price: -1 }
+        : order === 'toprated'
+        ? { rating: -1 }
+        : order === 'newest'
+        ? { createdAt: -1 }
+        : { _id: -1 };
+
+    const products = await Product.find({
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    })
+      .sort(sortOrder)
+      .skip(pageSize * (page - 1))
+      .limit(pageSize);
+
+    const countProducts = await Product.countDocuments({
+      ...queryFilter,
+      ...categoryFilter,
+      ...priceFilter,
+      ...ratingFilter,
+    });
+
+    res.send({
+      products,
+      countProducts,
+      page,
+      pages: Math.ceil(countProducts / pageSize),
+    });
+  })
+);
+
+productRoute.get(
+  '/categories',
+  expressAsyncHandler(async (req, res) => {
+    const categories = await Product.find().distinct('category');
+    res.send(categories);
+  })
+);
 
 productRoute.get('/slug/:slug', async (req, res) => {
   const product = await Product.findOne({ slug: req.params.slug });
--- MyE-commerce/frontend/src/App.js
+++ MyE-commerce/frontend/src/App.js
@@ -1,10 +1,17 @@
-import { Badge, Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import {
+  Badge,
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from 'react-bootstrap';
 
-import { ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ProductScreen from './screens/ProductScreen';
 import { Store } from './Store';
 import CartsScreen from './screens/CartsScreen';
@@ -17,10 +24,16 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import axios from 'axios';
+import { getError } from './utils';
+import SearchBox from './component/SearchBox';
+import SearchScreen from './screens/SearchScreen';
 function App() {
   const { state, dispatch: ctxdispatcher } = useContext(Store);
   const { cart, userinfo } = state;
 
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
   const signoutHandler = () => {
     ctxdispatcher({ type: 'SIGN_OUT' });
     localStorage.removeItem('userInfo');
@@ -29,18 +42,43 @@ function App() {
     window.location.href = '/signin';
   };
 
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const { data } = await axios.get(`/api/products/categories`);
+        setCategories(data);
+      } catch (err) {
+        toast.error(getError(err));
+      }
+    };
+    fetchCategories();
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="d-flex flex-column site-container">
+      <div
+        className={
+          sidebarIsOpen
+            ? 'd-flex flex-column site-container active-cont'
+            : 'd-flex flex-column site-container '
+        }
+      >
         <ToastContainer position="bottom-center" limit={1}></ToastContainer>
         <header>
           <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
+              <Button
+                variant="dark"
+                onClick={() => setSidebarIsOpen(!sidebarIsOpen)}
+              >
+                <i className="fas fa-bars" />
+              </Button>
               <LinkContainer to="/">
                 <Navbar.Brand>Logo</Navbar.Brand>
               </LinkContainer>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
+                <SearchBox />
                 <Nav className="me-auto w-100 justify-content-end">
                   <Link className="nav-link" to="/cart">
                     Cart
@@ -77,11 +115,13 @@ function App() {
             </Container>
           </Navbar>
         </header>
+
         <main>
           <Container className="mt-3">
             <Routes>
               <Route path="/" element={<HomeScreen />}></Route>
               <Route path="/cart" element={<CartsScreen />} />
+              <Route path="/search" element={<SearchScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/profile" element={<ProfileScreen />} />
@@ -97,6 +137,32 @@ function App() {
             </Routes>
           </Container>
         </main>
+
+        <div
+          className={
+            sidebarIsOpen
+              ? 'active-nav side-navbar d-flex justify-content-between flex-wrap flex-column'
+              : 'side-navbar d-flex justify-content-between flex-wrap flex-column'
+          }
+        >
+          <Nav className="flex-column text-white w-100 p-2">
+            <Nav.Item>
+              <strong>Categories</strong>
+            </Nav.Item>
+            {categories.map((category) => {
+              return (
+                <Nav.Item key={category}>
+                  <LinkContainer
+                    to={`/search?category=${category}`}
+                    onClick={() => setSidebarIsOpen(false)}
+                  >
+                    <Nav.Link>{category}</Nav.Link>
+                  </LinkContainer>
+                </Nav.Item>
+              );
+            })}
+          </Nav>
+        </div>
 
         <footer className="text-center">ALL RIGHT RESERVED</footer>
       </div>
